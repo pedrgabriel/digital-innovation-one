e77682752783a221de3ada3bc436b56247517e8e
pedrgabriel <pedrgabriel_@outlook.com>
2022-06-30 14:22:23 -0300

    Private view modifier assignment in set function of cpf property

--- src/main/kotlin/one/digitalinnovation/bank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/bank/Pessoa.kt
@@ -3,11 +3,12 @@ package one.digitalinnovation.bank
 class Pessoa {
     var nome: String = "Pedro"
     var cpf: String = "123.456.789-00"
+    private set
 }
 
 fun main() {
     val pedro = Pessoa()
-
+    println(pedro)
     println(pedro.nome)
     println(pedro.cpf)
 }
