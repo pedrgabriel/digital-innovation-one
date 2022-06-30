668b836fd10890ef3be4dd140f48041c3efc19cc
pedrgabriel <pedrgabriel_@outlook.com>
2022-06-30 15:04:38 -0300

    Creation of the account class

---
+++ src/main/kotlin/one/digitalinnovation/bank/Conta.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.bank
+import java.math.BigDecimal
+
+class Conta (
+    val numero: String,
+    val agencia: String,
+    val saldo: BigDecimal,
+) {
+    fun deposito(valor: BigDecimal) {
+
+    }
+    fun saque(valor: BigDecimal) {
+
+    }
+}
