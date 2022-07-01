b7b02dff0966e8a0a0e0a58e33e5d003c1633bf0
pedrgabriel <pedrgabriel_@outlook.com>
2022-07-01 15:08:08 -0300

    Creation of the employee class and update on the person class implementing class abstraction

---
+++ src/main/kotlin/one/digitalinnovation/bank/Funcionario.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.bank
+import java.math.BigDecimal
+
+abstract class Funcionario(
+    nome: String,
+    cpf: String,
+    val salario: BigDecimal
+) : Pessoa(nome, cpf) {
+    abstract fun calculoAuxilio();
+}
--- src/main/kotlin/one/digitalinnovation/bank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/bank/Pessoa.kt
@@ -1,15 +1,6 @@
 package one.digitalinnovation.bank
 
-class Pessoa {
-    var nome: String = "Pedro"
-    var cpf: String = "123.456.789-00"
-    private set
-
-    constructor()
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val pedro = Pessoa()
-    println(pedro.pessoaInfo())
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String,
+)
