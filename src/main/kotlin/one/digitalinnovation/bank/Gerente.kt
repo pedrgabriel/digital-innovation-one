0cb361b3cb8e6f7c858bbd2623987de653e87774
pedrgabriel <pedrgabriel_@outlook.com>
2022-07-06 10:25:51 -0300

    Creation of the analyst and manager subclasses

---
+++ src/main/kotlin/one/digitalinnovation/bank/Gerente.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.bank
+
+class Gerente(
+    nome: String,
+    cpf: String,
+    salario: Double
+) : Funcionario(nome, cpf, salario) {
+    override fun calculoAuxilio(): Double = salario * 0.4
+}
