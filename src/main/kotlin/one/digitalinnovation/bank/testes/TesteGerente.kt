0874d5bf0e12b061c221a01c153509465fe3a96b
pedrgabriel <pedrgabriel_@outlook.com>
2022-07-06 11:50:07 -0300

    Update on manager class and test class

--- src/main/kotlin/one/digitalinnovation/bank/testes/TesteGerente.kt
+++ src/main/kotlin/one/digitalinnovation/bank/testes/TesteGerente.kt
@@ -2,6 +2,7 @@ package one.digitalinnovation.bank.testes
 import one.digitalinnovation.bank.Gerente
 
 fun main() {
-    val maria = Gerente("Maria", "111.222.333-44", 5000.00)
+val maria = Gerente("Maria", "111.222.333-44", 5000.00, "senha123")
     ImprimirRelatorioFuncionario.imprime(maria)
+    TesteAutenticacao().autenticaLogin(maria)
 }
